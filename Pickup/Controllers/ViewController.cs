d91f8f690fa0a789e7ecde34a38a888bf523fb32
Margaret Kiley <mlilliankiley@gmail.com>
2018-05-05 14:04:19 -0400

    Found a bug in my PickupOrDelivery table

---
+++ Pickup/Controllers/ViewController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Pickup.Data;
+using Pickup.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Pickup.Controllers
+{
+    public class ViewController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public ViewController(ApplicationDbContext applicationDbContext)
+        {
+            context = applicationDbContext;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index(int id)
+        {
+            PickupOrDelivery pickup = context.PickupsDeliveries.Where(p => p.ID == id).SingleOrDefault();
+            return View(pickup);
+        }
+    }
+}
---
+++ Pickup/Models/ViewViewModel/ViewInformationViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pickup.Models.ViewViewModel
+{
+    public class ViewInformationViewModel
+    {
+        public int PickupID { get; set; }
+        
+
+    }
+}
