471182a28f4b003bc2e3a5fae6379e8b2a410153
Margaret Kiley <mlilliankiley@gmail.com>
2018-05-20 16:00:16 -0400

    Moved View info to Home Controller

--- Pickup/Controllers/HomeController.cs
+++ Pickup/Controllers/HomeController.cs
@@ -5,28 +5,77 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Pickup.Data;
 using Pickup.Models;
+using Pickup.Models.HomeViewModel;
 
 namespace Pickup.Controllers
-{
+    {
     public class HomeController : Controller
-    {           public IActionResult Index()
+    {
+        private readonly ApplicationDbContext context;
+
+        public HomeController(ApplicationDbContext applicationDbContext)
+        {
+            context = applicationDbContext;
+        }
+
+        public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult About()
+        [Route("/View")]
+        public IActionResult View(int id)
         {
-            ViewData["Message"] = "Your application description page.";
+            var furnitureItems = (from fpd in context.FurnitureDonationPickups
+                                  join f in context.Furniture on fpd.FurnitureID equals f.ID
+                                  where fpd.DonationPickupID == id
+                                  select new FurnitureListing
+                                  {
+                                      Name = f.Name,
+                                      ID = f.ID,
+                                      Quantity = fpd.Quantity,
+                                      Description = fpd.Description
+                                  }).ToList();
+            var listItems = new List<FurnitureListing>();
+            foreach (var item in furnitureItems)
+            {
+                listItems.Add(new FurnitureListing()
+                {
+                    ID = item.ID,
+                    Name = item.Name,
+                    Quantity = item.Quantity,
+                    Description = item.Description
 
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
+                });
+            }
+            var results = (from p in context.PickupsDeliveries
+                           join s in context.Users on p.UserId equals s.Id
+                           join a in context.Addresses on p.AddressID equals a.ID
+                           join d in context.DonorsCustomers on a.DonorCustomerID equals d.ID
+                           join fpd in context.FurnitureDonationPickups on p.ID equals fpd.DonationPickupID
+                           where p.ID == id
+                           select new ViewInformationViewModel()
+                           {
+                               FirstName = d.FirstName,
+                               LastName = d.LastName,
+                               PhoneNumber = d.PhoneNumber,
+                               PhoneNumberTwo = d.PhoneNumberTwo,
+                               StreetAddress = a.Street,
+                               Apt = a.Apartment,
+                               City = a.City,
+                               ZIP = a.ZIP,
+                               Neighborhood = a.Neighborhood,
+                               Delivery = p.Delivery,
+                               ScheduleDateTime = p.ScheduleDateTime,
+                               CallEnRoute = p.CallEnRoute,
+                               SpecialInstructions = p.SpecialInstructions,
+                               PickupDateTime = p.PickupDateTime,
+                               Scheduler = s.FullName,
+                               Furniture = listItems
+                           }).FirstOrDefault();
+            return View(results);
         }
 
         public IActionResult Error()
--- Pickup/Models/HomeViewModel/ViewInformationViewModel.cs
+++ Pickup/Models/HomeViewModel/ViewInformationViewModel.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Pickup.Models.ViewViewModel
+namespace Pickup.Models.HomeViewModel
 {
     public class ViewInformationViewModel
     {
