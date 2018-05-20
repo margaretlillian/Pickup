c9ffae46717d852910c98e05ac6bfa73e5e6dee1
Margaret Kiley <mlilliankiley@gmail.com>
2018-05-20 16:12:28 -0400

    Created CancelEditPickupDelivery Controller (that's a mouthful)

---
+++ Pickup/Controllers/CancelEditPickupDeliveryController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Pickup.Data;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Pickup.Controllers
+{
+    public class CancelEditPickupDeliveryController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public CancelEditPickupDeliveryController(ApplicationDbContext applicationDbContext)
+        {
+            context = applicationDbContext;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
