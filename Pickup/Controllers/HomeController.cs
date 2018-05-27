4907aec32a2e477fb79cedf7101106e93af74607
Margaret Kiley <mlilliankiley@gmail.com>
2018-05-27 16:06:38 -0400

    Making the Views and popup versions more DRY

--- Pickup/Controllers/HomeController.cs
+++ Pickup/Controllers/HomeController.cs
@@ -32,7 +32,7 @@ namespace Pickup.Controllers
         }
 
         [Route("/View")]
-        public IActionResult View(int id)
+        public IActionResult View(int id, bool popup)
         {
             PickupOrDelivery individual = context.PickupsDeliveries.Where(p => p.ID == id).FirstOrDefault();
             if (individual == null)
@@ -40,70 +40,12 @@ namespace Pickup.Controllers
 
             ViewInformationViewModel results = query.CreateQuery(context, id);
             results.Furniture = query.CreateFurnitureListQuery(context, id);
+
+            if (popup)
+                return View("ViewPopup", results);
             return View(results);
         }
-
-
-        [Route("/ViewPopup")]
-        public IActionResult ViewPopup(int id)
-        {
-            PickupOrDelivery individual = context.PickupsDeliveries.Where(p => p.ID == id).FirstOrDefault();
-            if (individual == null)
-                return Redirect("/");
-
-            var furnitureItems = (from fpd in context.FurnitureDonationPickups
-                                  join f in context.Furniture on fpd.FurnitureID equals f.ID
-                                  where fpd.DonationPickupID == id
-                                  select new FurnitureListing
-                                  {
-                                      Name = f.Name,
-                                      ID = f.ID,
-                                      Quantity = fpd.Quantity,
-                                      Description = fpd.Description
-                                  }).ToList();
-            var listItems = new List<FurnitureListing>();
-            foreach (var item in furnitureItems)
-            {
-                listItems.Add(new FurnitureListing()
-                {
-                    ID = item.ID,
-                    Name = item.Name,
-                    Quantity = item.Quantity,
-                    Description = item.Description
-
-                });
-
-            }
-
-            ViewInformationViewModel results = (from p in context.PickupsDeliveries
-                                                join s in context.Users on p.UserId equals s.Id
-                                                join a in context.Addresses on p.AddressID equals a.ID
-                                                join d in context.DonorsCustomers on a.DonorCustomerID equals d.ID
-                                                where p.ID == id
-                                                select new ViewInformationViewModel()
-                                                {
-                                                    FirstName = d.FirstName,
-                                                    LastName = d.LastName,
-                                                    PhoneNumber = d.PhoneNumber,
-                                                    PhoneNumberTwo = d.PhoneNumberTwo,
-                                                    StreetAddress = a.Street,
-                                                    Apt = a.Apartment,
-                                                    City = a.City,
-                                                    ZIP = a.ZIP,
-                                                    Neighborhood = a.Neighborhood,
-                                                    Delivery = p.Delivery,
-                                                    ScheduleDateTime = p.ScheduleDateTime,
-                                                    CallEnRoute = p.CallEnRoute,
-                                                    SpecialInstructions = p.SpecialInstructions,
-                                                    PickupDateTime = p.PickupDateTime,
-                                                    Scheduler = s.FullName,
-                                                    PickupID = p.ID,
-                                                    Cancelled = p.Cancelled,
-                                                }).FirstOrDefault();
-
-            results.Furniture = listItems;
-            return View(results);
-        }
+        
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
