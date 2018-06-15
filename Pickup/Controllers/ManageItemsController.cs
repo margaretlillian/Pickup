be6031ca8fc4e58ef248d48bc108e4a113effa26
Margaret Kiley <mlilliankiley@gmail.com>
2018-06-15 08:15:50 -0400

    Created working add categories

--- Pickup/Controllers/ManageItemsController.cs
+++ Pickup/Controllers/ManageItemsController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Pickup.Data;
+using Pickup.Models;
+using Pickup.Models.ManageItemsViewModels;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,10 +13,39 @@ namespace Pickup.Controllers
 {
     public class ManageItemsController : Controller
     {
+
+        private readonly ApplicationDbContext context;
+
+        public ManageItemsController(ApplicationDbContext applicationDbContext)
+        {
+            context = applicationDbContext;
+        }
         // GET: /<controller>/
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult CreateCategory()
+        {
+            return View(new CreateCategoryViewModel());
+        }
+
+        [HttpPost]
+        public IActionResult CreateCategory(CreateCategoryViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ItemCategory newCategory = new ItemCategory()
+                {
+                    Name = model.Name
+                };
+                context.Add(newCategory);
+                context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            return View(model);
         }
     }
 }
